5631a9642d794ba08da8bd1aa19bbec21b7eaa47
GlitchDevX <jonas.ringeis@gmail.com>
2025-06-20 13:46:24 +0200

    Added env parameter & newTag default to latest

--- app/dto/requests/redeploy_container_data.py
+++ app/dto/requests/redeploy_container_data.py
@@ -3,7 +3,8 @@ from pydantic import BaseModel
 
 class RedeployContainerData(BaseModel):
     containerId: str
-    newTag: str
+    newTag: str = "latest"
+    environment: list[str] = []
 
     healthCheck: bool = False
     healthcheckTimeout: float = 30
