d11311898311bde8123ad7e796180acb65dc16c8
GlitchDevX <jonas.ringeis@gmail.com>
2025-06-23 22:49:35 +0200

    Built test base

---
+++ tests/routers/test_redeploy.py
@@ -0,0 +1,19 @@
+import docker
+import pytest
+from pytest_mock import MockerFixture
+
+from app.dto.requests.redeploy_container_data import RedeployContainerData
+from app.routers.redeploy import redeploy_container
+
+def test_redeploy_container(mocker: MockerFixture):
+    test_data = RedeployContainerData(containerId="testContainerId", newTag="someVersion", environment=["DB_PWD=SecurePassword"])
+    
+    daemon_mock = mocker.patch("docker.from_env")
+
+    # daemon_mock.return_value.containers.run.return_value = { "id": "containerId" }
+    
+    redeploy_container(test_data)
+
+    # daemon_mock.containers.run.assert_called_once()
+
+    # docker.with_daemon.assert_called_once()
