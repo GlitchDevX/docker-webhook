793e134e02f02f4ec6916692b9129b76dcbd3ba6
GlitchDevX <jonas.ringeis@gmail.com>
2025-06-25 12:54:05 +0200

    Added secret check middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+
+from app.middleware.secret_check import SecretCheck
 from .routers import containers, redeploy
 
 app = FastAPI()
 
 app.include_router(containers.router)
 app.include_router(redeploy.router)
+
+app.add_middleware(SecretCheck)
---
+++ app/middleware/secret_check.py
@@ -0,0 +1,28 @@
+import os
+from typing import Any, Awaitable, Callable, MutableMapping
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware, DispatchFunction, RequestResponseEndpoint
+from starlette.responses import Response
+
+class SecretCheck(BaseHTTPMiddleware):
+    def __init__(self, app: Callable[[MutableMapping[str, Any], Callable[[], Awaitable[MutableMapping[str, Any]]], Callable[[MutableMapping[str, Any]], Awaitable[None]]], Awaitable[None]], dispatch: Callable[[Request, Callable[[Request], Awaitable[Response]]], Awaitable[Response]] | None = None) -> None:
+        super().__init__(app, dispatch)
+        self._get_secret()
+
+    async def dispatch(self, request: Request, call_next: Callable[[Request], Awaitable[Response]]) -> Response:
+        if not self._verifiy_secret(request):
+            return JSONResponse(status_code=403, content="Forbidden")
+        return await call_next(request)
+
+    def _verifiy_secret(self, request: Request):
+        received_secret = request.headers.get("X-Api-Secret")
+        if received_secret is None or received_secret != self.secret:
+            return False
+        return True
+        
+    def _get_secret(self):
+        self.secret = os.environ.get("API_SECRET")
+        if self.secret is None:
+            print("No secret found in environment variable 'API_SECRET', will use 'ADD_PASSWORD_BEFORE_PRODUCTION'")
+            self.secret = "ADD_PASSWORD_BEFORE_PRODUCTION"
