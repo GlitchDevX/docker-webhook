ab3c85155923ac39c8212a6e3dcf3bc5b6719317
GlitchDevX <jonas.ringeis@gmail.com>
2025-06-25 11:57:05 +0200

    Renamed utils dir to common

--- app/common/container_utils.py
+++ app/common/container_utils.py
@@ -1,8 +1,9 @@
 import docker
-from fastapi import HTTPException
+
+from app.common.exceptions import container_not_found
 
 def try_get_container(daemon: docker.DockerClient, container_id: str):
     try:
         return daemon.containers.get(container_id)
     except:
-        raise HTTPException(404, "Container with id '{container_id}' was not found")
+        raise container_not_found(container_id)
