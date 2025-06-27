9a42ae75a96c904f726c3429662d42d3087c21bf
GlitchDevX <jonas.ringeis@gmail.com>
2025-06-27 09:57:54 +0200

    Fixed typo & added await before async call

--- app/dto/requests/redeploy_image_data.py
+++ app/dto/requests/redeploy_image_data.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 
-class RedepoyImageData(BaseModel):
+class RedeployImageData(BaseModel):
     imageName: str
     newTag: str = "latest"
     allowNew: bool = True # allows creation of new container if non is existing
